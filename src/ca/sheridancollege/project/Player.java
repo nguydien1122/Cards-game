b9e559b3406ec701f1ad82381062258d414d00c2
nguydien1122 <46941692+nguydien1122@users.noreply.github.com>
2019-03-12 21:36:00 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,7 +8,7 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author dancye, 2018
- * Team members: Nitee Sharma(991511406), Vinit Patel, Dien Nguyen(991501600)
+ * Team members: Nitee Sharma(991511406), Vinit Patel(991510686), Dien Nguyen(991501600)
  */
 public abstract class Player 
 {
